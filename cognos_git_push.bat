c8f69eab2ad4434773a45defeb631090a147e29f
notijohn <54985296+notijohn@users.noreply.github.com>
2019-10-07 15:35:55 +0530

    Cognos source files

---
+++ cognos_git_push.bat
@@ -0,0 +1,49 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+
+SET "repoBase="
+SET "qlikBase=C:\Program Files\ibm\cognos\analytics\deployment"
+::SET "qlikFile=Sales.qvf"
+
+SET "InvokeDesktop=YES"
+SET qvfSrc="%qlikBase%%qlikFile%"
+
+SET "gitPush=YES"
+
+SET gitrepo="https://notijohn:qw3rty4321!@github.com/notijohn/cognos.git"
+
+SET "applogs=event.log"
+SET "qlikExe=C:\Users\amit.u.sharma\AppData\Local\Programs\Qlik\Sense\QlikSense.exe"
+::SET "gitExe="
+::SET "nodeExe=C:\Program Files\nodejs\node.exe"
+SET "nodeCode=C:\Users\amit.u.sharma\Desktop\qliksense\Automation\QlikSenseDevOps"
+SET "workenv=C:\Users\amit.u.sharma\Desktop\qliksense\workspace"
+
+:: Dont edit below this 
+
+::echo exec node command
+::cd %nodeCode%
+::git pull 
+::"%nodeExe%" node_modules/qs-version-control-dev to-json -c config_demo.json
+
+set mydate=!date:~10,4!!date:~6,3!/!date:~4,2!
+
+echo commiting changes to git
+
+cd C:\Program Files\ibm\cognos\analytics\deployment\
+git add .
+git commit -m "New files to upload"
+git push --all https://github.com/notijohn/cognos.git
+::git push --all %gitrepo%
+set /p version_id=Enter version_id: 
+
+:: Git Tagging
+
+git tag %version_id%.%mydate%  
+git push https://github.com/notijohn/cognos.git %version_id%.%mydate%   
+::git push %gitrepo% %version_id%.%mydate%   
+ 
+pause    
+
+exit
---
+++ validation.py
@@ -0,0 +1,103 @@
+import glob
+import os
+from time import sleep
+import getpass
+from connection import hiv 
+import pandas as pd
+from selenium.webdriver import ActionChains
+import pyodbc
+import numpy as np
+import numpy.testing as npt
+import pyautogui
+from selenium.webdriver.common.keys import Keys
+
+input = pd.ExcelFile("Input.xlsx")
+report = input.parse("reportname")
+reportname = report.at[0,'Report Name']
+db = input.parse("database")
+dbtype = db.at[0,'Database']
+user = getpass.getuser()
+
+path = "http://acitgdprwn01:9300/bi/?perspective=home"
+
+
+def validate(self):
+    input = pd.ExcelFile("Input.xlsx")
+    report = input.parse("reportname")
+    reportname = report.at[0, 'Report Name']
+    n = self.driver.find_element_by_xpath('//*[@id="com.ibm.bi.contentApps.myContentFoldersSlideout"]')
+    n.click()
+    sleep(10)
+    #self.driver.find_element_by_xpath('//*[contains(text(),' +str(reportname)+')]').click()
+    self.driver.find_element_by_xpath("//*[contains(text(), 'Policy')]").click()
+    sleep(100)
+    self.driver.find_element_by_xpath('//*[@id="com.ibm.bi.authoring.runBtn.menu"]').click()
+    sleep(5)
+    self.driver.find_element_by_xpath("//*[contains(text(), 'Run Excel')]").click()
+    sleep(20)
+    
+
+
+    # comparing the 2 excel sheets
+    df_OLD = pd.read_excel("actuals.xlsx").fillna(0)
+
+    try:
+        path_NEW = glob.glob(os.path.join('C:/Users/' + str(user) + '/Downloads', "*.xlsx"))[0]
+    except IndexError:
+        raise IOError("No .xlsx files found in ")
+    finally:
+        df_NEW = pd.read_excel(path_NEW).fillna(0)
+
+    dfDiff = df_OLD.copy()
+    error_count = 0
+    for row in range(dfDiff.shape[0]):
+        for col in range(dfDiff.shape[1]):
+            value_OLD = df_OLD.iloc[row, col]
+            try:
+                value_NEW = df_NEW.iloc[row, col]
+                if value_OLD == value_NEW:
+                    dfDiff.iloc[row, col] = df_NEW.iloc[row, col]
+                else:
+                    #dfDiff.iloc[row, col] = ('{}-->{}').format(value_OLD, value_NEW)
+                    dfDiff.iloc[row, col] = "Error"
+                    error_count = error_count + 1
+            except:
+                dfDiff.iloc[row, col] = "NaN"
+
+    fname = "excel_diff.xlsx"
+    writer = pd.ExcelWriter(fname, engine='xlsxwriter')
+
+    dfDiff.to_excel(writer, sheet_name='DIFF', index=False)
+    df_NEW.to_excel(writer, sheet_name="report data", index=False)
+    df_OLD.to_excel(writer, sheet_name="actual data", index=False)
+
+    workbook = writer.book
+    worksheet = writer.sheets['DIFF']
+    worksheet.hide_gridlines(2)
+
+    # define formats
+    grey_fmt = workbook.add_format({'font_color': '#030303'})
+    highlight_fmt = workbook.add_format({'font_color': '#e61515', 'bg_color': '#e61515'})
+
+    ## highlight changed cells
+    worksheet.conditional_format('A1:ZZ1000', {'type': 'text',
+                                               'criteria': 'containing',
+                                               'value': '→',
+                                               'format': highlight_fmt})
+    ## highlight unchanged cells
+    worksheet.conditional_format('A1:ZZ1000', {'type': 'text',
+                                               'criteria': 'not containing',
+                                               'value': '→',
+                                               'format': grey_fmt})
+    # save
+    writer.save()
+
+    if error_count > 0:
+        print("Report Testing FAILED")
+    else:
+        print("Report Testing PASSED")
+    
+    self.assertEqual(0, error_count, "Report Testing FAILED")
+
+    os.remove(glob.glob(os.path.join('C:/Users/' + str(user) + '/Downloads', "*.xlsx"))[0])
+    #exit()
